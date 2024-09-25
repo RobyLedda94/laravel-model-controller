05c0e46f6fdbc542ada8ebb941c7cbc4122779cb
RobyLedda94 <robyledda01@yahoo.it>
2024-09-25 15:55:47 +0200

    [FIX] posizionato testo nel tag a

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -6,9 +6,9 @@
                 </div>
                 <div class="col-12">
                     <ul class="list-unstyled d-flex justify-content-center">
-                        <li class="px-3 py-3"><a class="nav-link" href="{{route('homepage')}}"></a>Home</li>
-                        <li class="px-3 py-3"><a class="nav-link" href="#"></a>Movies</li>
-                        <li class="px-3 py-3"><a class="nav-link" href="#"></a>Contacts</li>
+                        <li class="px-3 py-3"><a class="nav-link" href="{{route('homepage')}}">Home</a></li>
+                        <li class="px-3 py-3"><a class="nav-link" href="#">Movies</a></li>
+                        <li class="px-3 py-3"><a class="nav-link" href="#">Contacts</a></li>
                     </ul>
                 </div>
             </div>
