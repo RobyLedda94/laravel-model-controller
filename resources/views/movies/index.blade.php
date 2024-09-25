3c5ca91462a0d33f019af23ae39927b51c0746c1
RobyLedda94 <robyledda01@yahoo.it>
2024-09-25 18:27:43 +0200

    [UPD] definito leggermente lo stile alle card

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,19 +5,19 @@
         <div class="row">
             <div class="col-12">
                 <div class="content-films text-center">
-                    <h2 class="my-5">{{$title}}</h2>
+                    <h2 class="my-4">{{$title}}</h2>
                     <p class="mb-3">{{$description}}</p>
                 </div>
                 @foreach($movies as $movie)
                 <div class="col-12">
                     <div class="card my-2">
                         <div class="card-body">
-                            <div class="card-title">{{ $movie->id }}</div>
-                            <div class="card-subtitle">{{ $movie->title }}</div>
-                            <p class="card-text">{{$movie->original_title}}</p>
-                            <p class="card-text">{{$movie->nationality}}</p>
-                            <p class="card-text">{{$movie->date}}</p>
-                            <p class="card-text">{{$movie->vote}}</p>
+                            <div class="card-id p-3 mb-3 rounded"><strong>{{ $movie->id }}.</strong></div>
+                            <h3 class="card-subtitle mb-2">Titolo: {{ $movie->title }}</h3>
+                            <p class="card-text">Sottotitolo: {{$movie->original_title}}</p>
+                            <p class="card-text">Nazionalità : {{$movie->nationality}}</p>
+                            <p class="card-text">Anno: {{$movie->date}}</p>
+                            <p class="card-text">Voto: {{$movie->vote}}</p>
                         </div>
                     </div>
                 </div>
