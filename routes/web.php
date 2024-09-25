fd1cb050c91ed2f71fc9d300424536a2ab57cd90
RobyLedda94 <robyledda01@yahoo.it>
2024-09-25 17:35:15 +0200

    [UPD] creato index.blade, definita la rotta ed individuato la classe MovieController

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 // dico al file web.php dove trovare la classe PageController
 use App\Http\Controllers\PageController as PageController;
+// dico al file web.php dove trovare la classe MovieController
+use App\Http\Controllers\MovieController as MovieController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -24,5 +27,7 @@ use App\Http\Controllers\PageController as PageController;
 Route::get('/', [PageController::class, 'home'])->name('homepage');
 // rotta per i contatti
 Route::get('/contacts', [PageController::class, 'contacts'])->name('contacts');
+// rotta per i movies
+Route::get('/movies', [MovieController::class, 'index'])->name('movies');
 
 
