8d6befe9365f100d914528520a69b8af3df61f3f
RobyLedda94 <robyledda01@yahoo.it>
2024-09-25 17:39:53 +0200

    [UPD] ok definita la rotta per la pagina dove saranno mostrati i film

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -7,7 +7,7 @@
                 <div class="col-12">
                     <ul class="list-unstyled d-flex justify-content-center">
                         <li class="px-3 py-3"><a class="nav-link" href="{{route('homepage')}}">Home</a></li>
-                        <li class="px-3 py-3"><a class="nav-link" href="#">Movies</a></li>
+                        <li class="px-3 py-3"><a class="nav-link" href="{{route('movies')}}">Movies</a></li>
                         <li class="px-3 py-3"><a class="nav-link" href="{{route('contacts')}}">Contacts</a></li>
                     </ul>
                 </div>
