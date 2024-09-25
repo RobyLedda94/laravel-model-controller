c450c3e71fb50d0f36c35df57b8497149d7f16d5
RobyLedda94 <robyledda01@yahoo.it>
2024-09-25 15:34:53 +0200

    [UPD] scaffholding base, creati partials header e layouts

---
+++ resources/views/home.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <h3>Lista dei Film :</h3>
+                <p>Qua saranno ciclati i dati che recupero dal DB</p>
+            </div>
+        </div>
+    </div>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,16 @@
+<header class="py-3">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <h1 class="text-center">Movies</h1>
+                </div>
+                <div class="col-12">
+                    <ul class="list-unstyled d-flex justify-content-center">
+                        <li class="px-3 py-3"><a class="nav-link" href="#"></a>Home</li>
+                        <li class="px-3 py-3"><a class="nav-link" href="#"></a>Movies</li>
+                        <li class="px-3 py-3"><a class="nav-link" href="#"></a>Contacts</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
